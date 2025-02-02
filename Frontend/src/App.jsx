4049052f185526d11e894b0bc6f393e61cf69f13
vikas <vikaszenithian@gmail.com>
2025-02-02 22:24:06 +0530

    full backend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div>
+      <h1 classname= "text-">Job Portal</h1>
+    </div>
+  )
+}
+
+export default App
+
---
+++ readme.md
@@ -0,0 +1,2 @@
+#Welcome to my Youtube Look Alike App
+Hello!! Lets create a Job Portal Application
